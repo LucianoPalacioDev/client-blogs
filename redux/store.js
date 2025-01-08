0fd834ca985fa51783a44fc276defb007976d609
Luciano <lucianopalacio@toptive.co>
2025-01-08 08:54:22 -0300

    feat: complete basic redux configuration

--- redux/store.js
+++ redux/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import blogsSlice from "@/redux/reducers/blogsSlice";
+import { blogsApi } from "@/redux/api/blogsApi";
 
 export const store = configureStore({
   reducer: {
     blogs: blogsSlice,
+    [blogsApi.reducerPath]: blogsApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(blogsApi.middleware),
 });
